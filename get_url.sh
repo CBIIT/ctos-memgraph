7cbf530ad9eaf4fc4335e205db098ab34afdc9b8
Joe Sislow <joseph.sislow@nih.gov>
2026-09-11 15:13:59 -0500

    FIX: Fix URLs
    - Fix URLs

--- get_url.sh
+++ get_url.sh
@@ -10,9 +10,9 @@ if [[ "$MEMGRAPH_VERSION" == "3.11" || "$MEMGRAPH_VERSION" == "3.12" ]]; then
 		fi
 	fi
 elif [[ "$MEMGRAPH_VERSION" == "3.13" ]]; then
-	DL_URL=https://download.memgraph.com/memgraph/v$MEMGRAPH_VERSION.0/x86_64-deb/memgraph_"$MEMGRAPH_VERSION".0-1.amd64.deb
+	DL_URL=https://download.memgraph.com/memgraph/v$MEMGRAPH_VERSION.0/deb/memgraph_"$MEMGRAPH_VERSION".0-1.amd64.deb
 	if [[ "$MEMGRAPH_TYPE" == "memgraph-mage" ]]; then
-		DL_URL="$DL_URL "https://download.memgraph.com/$MEMGRAPH_TYPE/v$MEMGRAPH_VERSION.0/x86_64-deb/"$MEMGRAPH_TYPE"_"$MEMGRAPH_VERSION".0-1.amd64.deb
+		DL_URL="$DL_URL "https://download.memgraph.com/$MEMGRAPH_TYPE/v$MEMGRAPH_VERSION.0/deb/"$MEMGRAPH_TYPE"_"$MEMGRAPH_VERSION".0-1.amd64.deb
 	fi
 else
 	echo "Unknown memgraph version $MEMGRAPH_VERSION"
