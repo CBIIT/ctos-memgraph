e68c37a9dfb5581abd8aa7082ed0ff4dff984312
Joe Sislow <joseph.sislow@nih.gov>
2026-09-11 16:05:49 -0500

    FIX: Fix URLs
    - Fix URLs

--- get_url.sh
+++ get_url.sh
@@ -4,15 +4,15 @@ UBUNTU_VERSION=$3
 
 if [[ "$MEMGRAPH_VERSION" == "3.11" || "$MEMGRAPH_VERSION" == "3.12" ]]; then
 	if [[ "$UBUNTU_VERSION" != "26.04" ]]; then
-		DL_URL=https://download.memgraph.com/memgraph/v$MEMGRAPH_VERSION.0/ubuntu-$UBUNTU_VERSION/memgraph_"$MEMGRAPH_VERSION".0-1.amd64.deb
+		DL_URL=https://download.memgraph.com/memgraph/v$MEMGRAPH_VERSION.0/ubuntu-$UBUNTU_VERSION/memgraph_"$MEMGRAPH_VERSION".0-1_amd64.deb
 		if [[ "$MEMGRAPH_TYPE" == "memgraph-mage" ]]; then
-			DL_URL="$DL_URL "https://download.memgraph.com/$MEMGRAPH_TYPE/v$MEMGRAPH_VERSION.0/ubuntu-$UBUNTU_VERSION/"$MEMGRAPH_TYPE"_"$MEMGRAPH_VERSION".0-1.amd64.deb
+			DL_URL="$DL_URL "https://download.memgraph.com/$MEMGRAPH_TYPE/v$MEMGRAPH_VERSION.0/ubuntu-$UBUNTU_VERSION/"$MEMGRAPH_TYPE"_"$MEMGRAPH_VERSION".0-1_amd64.deb
 		fi
 	fi
 elif [[ "$MEMGRAPH_VERSION" == "3.13" ]]; then
-	DL_URL=https://download.memgraph.com/memgraph/v$MEMGRAPH_VERSION.0/deb/memgraph_"$MEMGRAPH_VERSION".0-1.amd64.deb
+	DL_URL=https://download.memgraph.com/memgraph/v$MEMGRAPH_VERSION.0/deb/memgraph_"$MEMGRAPH_VERSION".0-1_amd64.deb
 	if [[ "$MEMGRAPH_TYPE" == "memgraph-mage" ]]; then
-		DL_URL="$DL_URL "https://download.memgraph.com/$MEMGRAPH_TYPE/v$MEMGRAPH_VERSION.0/deb/"$MEMGRAPH_TYPE"_"$MEMGRAPH_VERSION".0-1.amd64.deb
+		DL_URL="$DL_URL "https://download.memgraph.com/$MEMGRAPH_TYPE/v$MEMGRAPH_VERSION.0/deb/"$MEMGRAPH_TYPE"_"$MEMGRAPH_VERSION".0-1_amd64.deb
 	fi
 else
 	echo "Unknown memgraph version $MEMGRAPH_VERSION"
