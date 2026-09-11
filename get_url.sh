5e243b5b78f9d331af10caf27ad63cdd25c2e802
Joe Sislow <joseph.sislow@nih.gov>
2026-09-11 14:38:13 -0500

    ENH: Simplify URL script logic, return 2 results
    - Simplify URL generation script logic
    - Return 2 URLs for memgraph-mage

--- get_url.sh
+++ get_url.sh
@@ -2,52 +2,20 @@ MEMGRAPH_TYPE=$1
 MEMGRAPH_VERSION=$2
 UBUNTU_VERSION=$3
 
-if [[ "$MEMGRAPH_TYPE" == "memgraph" ]]; then
-	if [[ "$MEMGRAPH_VERSION" == "3.11" ]]; then
-		if [[ "$UBUNTU_VERSION" == "24.04" ]]; then
-			DL_URL=https://download.memgraph.com/memgraph/v3.11.0/ubuntu-24.04/memgraph_3.11.0-1_amd64.deb
-		elif [[ "$UBUNTU_VERSION" == "22.04" ]]; then
-			DL_URL=https://download.memgraph.com/memgraph/v3.11.0/ubuntu-22.04/memgraph_3.11.0-1_amd64.deb
-		else
-			echo "Unable to download $MEMGRAPH_TYPE - $MEMGRAPH_VERSION for $UBUNTU_VERSION"
+if [[ "$MEMGRAPH_VERSION" == "3.11" || "$MEMGRAPH_VERSION" == "3.12" ]]; then
+	if [[ "$UBUNTU_VERSION" != "26.04" ]]; then
+		DL_URL=https://download.memgraph.com/memgraph/v$MEMGRAPH_VERSION.0/ubuntu-$UBUNTU_VERSION/memgraph_"$MEMGRAPH_VERSION".0-1.amd64.deb
+		if [[ "$MEMGRAPH_TYPE" == "memgraph-mage" ]]; then
+			DL_URL="$DL_URL "https://download.memgraph.com/$MEMGRAPH_TYPE/v$MEMGRAPH_VERSION.0/ubuntu-$UBUNTU_VERSION/"$MEMGRAPH_TYPE"_"$MEMGRAPH_VERSION".0-1.amd64.deb
 		fi
-	elif [[ "$MEMGRAPH_VERSION" == "3.12" ]]; then
-		if [[ "$UBUNTU_VERSION" == "24.04" ]]; then
-			DL_URL=https://download.memgraph.com/memgraph/v3.12.0/ubuntu-24.04/memgraph_3.12.0-1_amd64.deb
-		elif [[ "$UBUNTU_VERSION" == "22.04" ]]; then
-			DL_URL=https://download.memgraph.com/memgraph/v3.12.0/ubuntu-22.04/memgraph_3.12.0-1_amd64.deb
-		else
-			echo "Unable to download $MEMGRAPH_TYPE - $MEMGRAPH_VERSION for $UBUNTU_VERSION"
-		fi
-	elif [[ "$MEMGRAPH_VERSION" == "3.13" ]]; then
-		DL_URL=https://download.memgraph.com/memgraph/v3.13.0/x86_64-deb/memgraph_3.13.0-1_amd64.deb
-	else
-		echo "Unavailable $MEMGRAPH_TYPE version $MEMGRAPH_VERSION"
 	fi
-elif [[ "$MEMGRAPH_TYPE" == "memgraph-mage" ]]; then
-	if [[ "$MEMGRAPH_VERSION" == "3.11" ]]; then
-		if [[ "$UBUNTU_VERSION" == "24.04" ]]; then
-			DL_URL=https://download.memgraph.com/memgraph-mage/v3.11.0/ubuntu-24.04/memgraph-mage_3.11.0-1_amd64.deb
-		elif [[ "$UBUNTU_VERSION" == "22.04" ]]; then
-			DL_URL=https://download.memgraph.com/memgraph-mage/v3.11.0/ubuntu-22.04/memgraph-mage_3.11.0-1_amd64.deb
-		else
-			echo "Unable to download $MEMGRAPH_TYPE - $MEMGRAPH_VERSION for $UBUNTU_VERSION"
-		fi
-	elif [[ "$MEMGRAPH_VERSION" == "3.12" ]]; then
-		if [[ "$UBUNTU_VERSION" == "24.04" ]]; then
-			DL_URL=https://download.memgraph.com/memgraph-mage/v3.12.0/ubuntu-24.04/memgraph-mage_3.12.0-1_amd64.deb
-		elif [[ "$UBUNTU_VERSION" == "22.04" ]]; then
-			DL_URL=https://download.memgraph.com/memgraph-mage/v3.12.0/ubuntu-22.04/memgraph-mage_3.12.0-1_amd64.deb
-		else
-			echo "Unable to download $MEMGRAPH_TYPE - $MEMGRAPH_VERSION for $UBUNTU_VERSION"
-		fi
-	elif [[ "$MEMGRAPH_VERSION" == "3.13" ]]; then
-		DL_URL=https://download.memgraph.com/memgraph-mage/v3.13.0/deb/memgraph-mage_3.13.0-1_amd64.deb
-	else
-		echo "Unavailable $MEMGRAPH_TYPE version $MEMGRAPH_VERSION" 
-	fi 
-else 
-	echo "Unavailable type $MEMGRAPH_TYPE" 
+elif [[ "$MEMGRAPH_VERSION" == "3.13" ]]; then
+	DL_URL=https://download.memgraph.com/memgraph/v$MEMGRAPH_VERSION.0/x86_64-deb/memgraph_"$MEMGRAPH_VERSION".0-1.amd64.deb
+	if [[ "$MEMGRAPH_TYPE" == "memgraph-mage" ]]; then
+		DL_URL="$DL_URL "https://download.memgraph.com/$MEMGRAPH_TYPE/v$MEMGRAPH_VERSION.0/x86_64-deb/"$MEMGRAPH_TYPE"_"$MEMGRAPH_VERSION".0-1.amd64.deb
+	fi
+else
+	echo "Unknown memgraph version $MEMGRAPH_VERSION"
 fi
 
 if [[ -z "$DL_URL" ]]; then
